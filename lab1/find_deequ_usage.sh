5e58b4edcea99ca9065305a16f646dffb7bb369f
Indika Kumara <ipkumarawd@yahoo.com>
2024-09-11 22:19:40 +0200

    Add One Exercise Template

---
+++ lab1/find_deequ_usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Step 1: install nbconvert library that can be used to covert jupyter notebooks to Python files
+
+
+# Step 2: create a director deequ and go to it
+
+
+# Step 3: clone Github repositories in repos.txt
+
+              
+# Step 4: Get the paths of all jupyter notebooks 
+
+
+# Step 5: Convert jupyter notebooks to  python files
+
+
+# Step 6: Merge all Python files into a single file
+
+
+# Step 7: Count the occurrence of each keyword in the merged Python file. keywords in keywords.txt 
---
+++ lab1/find_deequ_usage_without_notebooks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Step 1 : create a director deequ and go to it
+
+
+# Step 2: clone Github repositories in repos.txt
+
+
+# Step 3: Merge all Python files into a single file
+
+# Step 4: Count the occurrence of each keyword in the merged Python file. keywords in keywords.txt 
