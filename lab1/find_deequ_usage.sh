451ce232959f924b2b8784fbf06a19e3b95c4d68
Indika Kumara <ipkumarawd@yahoo.com>
2024-09-16 21:53:34 +0200

    Add Answers to Lab 1 exercises

--- lab1/find_deequ_usage.sh
+++ lab1/find_deequ_usage.sh
@@ -2,20 +2,45 @@
 
 # Step 1: install nbconvert library that can be used to covert jupyter notebooks to Python files
 
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install python3-dev python3-pip python3-venv -y
+ # Only create a virtual envrioment if it does not exist
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv 
+fi
 
-# Step 2: create a directory/folder deequ and go to it
+. .venv/bin/activate  # for why . instead of source see https://stackoverflow.com/questions/13702425/source-command-not-found-in-sh-shell
+pip install nbconvert
 
+# Step 2: create a director deequ and go to it
+mkdir deequ
+cd deequ
 
 # Step 3: clone Github repositories in repos.txt
 
+input="${1:-"../repos_subset.txt"}"  # Getting the repos lit from your input - the default value is ../repos_subset.txt
+while read -r repo; do
+  echo "${repo}.git"| git clone $repo
+done < "$input"
               
 # Step 4: Get the paths of all jupyter notebooks 
+find . -name '*ipynb' > "notebooks.txt" 
 
 
-# Step 5: Convert jupyter notebooks to  Python files
+# Step 5: Convert jupyter notebooks to  python files
 
+while read -r file; do
+  jupyter nbconvert --to script $file
+done < "notebooks.txt"
 
 # Step 6: Merge all Python files into a single file
 
+find . -name '*.py' -exec cat {} \; > all_deequ.py
+wc -l all_deequ.py
 
 # Step 7: Count the occurrence of each keyword in the merged Python file. keywords in keywords.txt 
+
+keywords_list="../keywords.txt"
+while read -r keyword; do
+  grep -oh "$keyword" all_deequ.py | wc -w | awk -v var="$keyword" '{printf "%s:%s\n",var,$1;}' >> ../results_deeque.txt
+done < "$keywords_list"
--- lab1/find_deequ_usage_without_notebooks.sh
+++ lab1/find_deequ_usage_without_notebooks.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Step 1: create a directory deequ and go to it
+# Step 1 : create a director deequ and go to it
 
+mkdir deequ
+cd deequ
 
-# Step 2: clone Github repositories in repos.txt
+# Step 2: clone Github repositories in repos.txt. Clone into a new directory, say deequ
 
+input="../repos.txt"
+while read -r repo; do
+  echo "${repo}.git"| git clone $repo
+done < "$input"
 
 # Step 3: Merge all Python files into a single file
 
+find . -name '*.py' -exec cat {} \; > all_deequ.py
+wc -l all_deequ.py
+
 # Step 4: Count the occurrence of each keyword in the merged Python file. keywords in keywords.txt 
+keywords_list="../keywords.txt"
+while read -r keyword; do
+  grep -oh "$keyword" all_deequ.py | wc -w | awk -v var="$keyword" '{printf "%s:%s\n",var,$1;}' >> ../results_deeque_without_notebooks.txt
+done < "$keywords_list"
