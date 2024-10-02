1c3b8467b97666cf68b4c5376946f3f931bdcbcb
Indika Kumara <ipkumarawd@yahoo.com>
2024-10-02 22:16:46 +0200

    Upgraded all to python:3.12.7

--- lab2/prediction-ui-embedded/runapp.sh
+++ lab2/prediction-ui-embedded/runapp.sh
@@ -1,11 +1,18 @@
-sudo apt update
+#!/bin/bash
 
-sudo apt install python3.10-venv
+# Step 1: install nbconvert library that can be used to covert jupyter notebooks to Python files
 
-python3 -m venv .myvenv
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install python3-dev python3-pip python3-venv python3-setuptools -y
 
-. .myvenv/bin/activate
+ # Only create a virtual envrioment if it does not exist
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv 
+fi
 
+. .venv/bin/activate  # for why . instead of source see https://stackoverflow.com/questions/13702425/source-command-not-found-in-sh-shell
+
+pip install --upgrade setuptools
 pip install -r requirements_dev.txt
 
-python -m pytest --junitxml=test_log.xml ./tests
+python3 -m pytest --junitxml=test_log.xml ./tests
